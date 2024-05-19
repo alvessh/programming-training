9875e78aa194d5766b8495bfe7430ee246f3e0f3
alvessh <117163985+alvessh@users.noreply.github.com>
2024-05-19 12:31:35 -0400

    adiciona o algoritmo para converter temperatura

---
+++ desafios/src/br/com/desafio/ConverteTemperatura.java
@@ -0,0 +1,20 @@
+package br.com.desafio;
+
+public class ConverteTemperatura {
+	public static void main(String[] args) {
+		System.out.println(converteCelsiusParaFahrenheit(10.0));
+		System.out.println(converteFahrenheitParaCelsius(50.0));
+	}
+	
+	// converter Celsius para Fahrenheit
+	public static Double converteCelsiusParaFahrenheit(Double celsius) {
+		Double calculo = (celsius * 1.8) + 32;
+		return calculo;
+	}
+
+	// converter Fahrenheit para Celsius
+	public static Double converteFahrenheitParaCelsius(Double fahrenheit) {
+		Double calculo = (fahrenheit - 32) / 1.8;
+		return calculo;
+	}
+}
