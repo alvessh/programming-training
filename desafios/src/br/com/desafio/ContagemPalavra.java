2127b2d9f972b3b61c1cdceb4d695167437c3b0f
alvessh <117163985+alvessh@users.noreply.github.com>
2024-05-19 13:11:13 -0400

    adiciona algoritmo para contagem de palavras

---
+++ desafios/src/br/com/desafio/ContagemPalavra.java
@@ -0,0 +1,44 @@
+package br.com.desafio;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ContagemPalavra {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite a frase:");
+		String frase = scanner.nextLine();
+
+		Map<String, Integer> contagem = contarPalavrasUnicas(frase);
+
+		for (String palavra : contagem.keySet()) {
+			System.out.println(palavra + ": " + contagem.get(palavra));
+		}
+
+		scanner.close();
+	}
+
+	public static Map<String, Integer> contarPalavrasUnicas(String frase) {
+		Map<String, Integer> mapContagemPalavra = new HashMap<>();
+
+		// Divide a frase em palavras usando espaço como delimitador
+		String[] palavras = frase.split(" ");
+
+		for (String palavra : palavras) {
+		
+			palavra = palavra.toLowerCase();
+
+			// Se a palavra já estiver no mapa, incrementa sua contagem,
+			//caso contrário, adiciona ao mapa com contagem 1
+			if (mapContagemPalavra.containsKey(palavra)) {
+				mapContagemPalavra.put(palavra, mapContagemPalavra.get(palavra) + 1);
+			} else {
+				mapContagemPalavra.put(palavra, 1);
+			}
+		}
+
+		return mapContagemPalavra;
+	}
+}
