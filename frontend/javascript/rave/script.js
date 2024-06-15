21c340e367ae88b493d652bda800470c2f0f7af6
alvessh <117163985+alvessh@users.noreply.github.com>
2024-06-15 17:46:24 -0400

    adiciona imagens na rave

--- frontend/javascript/rave/script.js
+++ frontend/javascript/rave/script.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const colors = [
-        "#FF5733", "#33FF57", "#3357FF", "#FF33A1", "#33FFF2",
-        "#F2FF33", "#A133FF", "#FF8C33", "#33FF8C", "#3380FF",
-        "#FF5733", "#57FF33", "#FF3357", "#33A1FF", "#F2A133",
-        "#8C33FF", "#FF33D1", "#33FFD1", "#D1FF33", "#5733FF"
-    ];
 
     // Gerar um array de 20 cores aleatórias
-    const colors1 = Array.from({ length: 50 }, getRandomColor);
-    console.log(colors1);
+    const colors = Array.from({ length: 20 }, getRandomColor);
+
+    // Array de imagens de pessoas na rave
+    const images = [
+        'image01.jpg',
+        'image02.jpg',
+        'image03.jpg',
+        'image04.jpg'
+    ];
 
     let currentColor = 0;
+    let currentImage = 0;
 
+    // Função para mudar a cor de fundo do body
     function changeBackgroundColor() {
-        document.body.style.backgroundColor = colors1[currentColor];
-        currentColor = (currentColor + 1) % colors1.length;
+        document.body.style.backgroundColor = colors[currentColor];
+        currentColor = (currentColor + 1) % colors.length;
     }
 
-    setInterval(changeBackgroundColor, 500);
-
-    // const audio = document.getElementById('rave-audio');
-    // audio.play();
+    // Função para mudar a imagem da rave
+    function changeImage() {
+        const imageElement = document.getElementById('rave-image');
+        imageElement.src = images[currentImage];
+        currentImage = (currentImage + 1) % images.length;
+    }
 
     // Selecionar o áudio e o botão
     const audio = document.getElementById('rave-audio');
@@ -31,9 +36,9 @@ document.addEventListener("DOMContentLoaded", function() {
         audio.play();
         button.style.display = 'none'; // Esconder o botão após o clique
         setInterval(changeBackgroundColor, 500); // Iniciar a mudança de cores
+        setInterval(changeImage, 1000); // Iniciar a mudança de imagens
     });
 });
-
 
 // Função para gerar uma cor aleatória em formato hexadecimal
 function getRandomColor() {
